5b98f2ae129292dbbf61309bad1fe98009d0a6f6
felipems999 <felipems97@hotmail.com>
2023-05-18 17:14:42 -0300

    Implementando função tam2()

--- src/config.py
+++ src/config.py
@@ -1,1 +1,7 @@
 IMG_DIR = "src/imagens/"
+
+CAR = IMG_DIR + "car.png"
+CROWD = IMG_DIR + "crowd.png"
+FRUIT1 = IMG_DIR + "fruit1.jpg"
+FRUIT2 = IMG_DIR + "fruit2.jpg"
+UNIVERSITY = IMG_DIR + "university.png"
--- src/quest_1.py
+++ src/quest_1.py
@@ -1,8 +1,30 @@
-import numpy
-import cv2
-from config import IMG_DIR
+import numpy as np
+import cv2 as cv
+from config import FRUIT1, FRUIT2
 
 
-def tam2():
-    img = cv2.imread(IMG_DIR + "car.png")
-    cv2.imshow(img, 1)
+def tam2(img, num: int = 4):
+    if num % 2 != 0 or num < 2:
+        num += 1
+
+    r, c, ch = img.shape
+    nr = r * 2
+    nc = c * 2
+    img_res = np.zeros((nr, nc, ch), dtype=np.uint8)
+
+    if num > 2:
+        print(f"num: {num}")
+        img_res = tam2(img_res, int(num / 2))
+    else:
+        return img_res
+
+
+img = cv.imread(FRUIT1)
+print("\nImagem Original\n")
+print(np.array(img))
+
+img_result = tam2(img, 4)
+print("\nImagem resultante\n")
+print(np.array(img_result))
+cv.imshow("Resultado", img_result)
+cv.waitKey(0)
