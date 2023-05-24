128940b062362cc7f1833e4aa0dd10260691cdf2
felipems999 <felipems97@hotmail.com>
2023-05-24 00:44:11 -0300

    Refazendo função tam2()

--- src/quest_1.py
+++ src/quest_1.py
@@ -8,9 +8,6 @@ def tam2(img, num: int = 8):
     if num % 2 != 0 or num < 2:
         num = 2 if num < 2 else num + 1
 
-    if num <= 2:
-        return img
-
     r, c, ch = img.shape
     nr = r * 2
     nc = c * 2
@@ -19,7 +16,10 @@ def tam2(img, num: int = 8):
         for j in range(nc):
             img_res[i, j] = img[i // 2, j // 2]
 
-    return tam2(img_res, num // 2)
+    if num <= 2:
+        return img_res
+    else:
+        return tam2(img_res, num // 2)
 
 
 def tamm(img):
@@ -68,7 +68,7 @@ def superres(img1, img2):
 img = cv.imread(FRUIT1)
 img2 = cv.imread(FRUIT2)
 
-img_tam2_8 = tam2(img, 4)
+img_tam2_8 = tam2(img, 8)
 img_tamm = tamm(img)
 img_superres = superres(img, img2)
 
@@ -76,8 +76,8 @@ cv.imwrite(RESULT_DIR + "q1_1_tam2(img, 8).jpg", img=img_tam2_8)
 cv.imwrite(RESULT_DIR + "q1_2_tamm(img).jpg", img=img_tamm)
 cv.imwrite(RESULT_DIR + "q1_3_superres(img, img2).jpg", img=img_superres)
 
-cv.imshow("original", img)
-cv.imshow("resultado", img_tam2_8)
+# cv.imshow("original", img)
+# cv.imshow("resultado", img_tam2_8)
 
-cv.waitKey(0)
-cv.destroyAllWindows()
+# cv.waitKey(0)
+# cv.destroyAllWindows()
